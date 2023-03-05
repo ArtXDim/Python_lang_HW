ffdec3ce753f73488d03ff87569a0decc1367ac3
Artemiev Dima <adbart1985@gmail.com>
2023-03-05 16:35:58 +0300

    pep8

--- HW/HW_3/task20.py
+++ HW/HW_3/task20.py
@@ -1,9 +1,9 @@
-# *Задача 20: * В настольной игре Скрабл (Scrabble) каждая буква имеет определенную ценность. 
+# *Задача 20: * В настольной игре Скрабл (Scrabble) каждая буква имеет определенную ценность.
 # В случае с английским алфавитом очки распределяются так:
-# A, E, I, O, U, L, N, S, T, R – 1 очко; D, G – 2 очка; B, C, M, P – 3 очка; F, H, V, W, Y – 4 очка; 
-# K – 5 очков; J, X – 8 очков; Q, Z – 10 очков. А русские буквы оцениваются так: А, В, Е, И, Н, О, Р, С, Т – 1 очко; 
+# A, E, I, O, U, L, N, S, T, R – 1 очко; D, G – 2 очка; B, C, M, P – 3 очка; F, H, V, W, Y – 4 очка;
+# K – 5 очков; J, X – 8 очков; Q, Z – 10 очков. А русские буквы оцениваются так: А, В, Е, И, Н, О, Р, С, Т – 1 очко;
 # Д, К, Л, М, П, У – 2 очка; Б, Г, Ё, Ь, Я – 3 очка; Й, Ы – 4 очка; Ж, З, Х, Ц, Ч – 5 очков; Ш, Э, Ю – 8 очков;
-# Ф, Щ, Ъ – 10 очков. 
+# Ф, Щ, Ъ – 10 очков.
 # Напишите программу, которая вычисляет стоимость введенного пользователем слова.
 # Будем считать, что на вход подается только одно слово, которое содержит либо только английские,
 # либо только русские буквы.
@@ -12,26 +12,31 @@
 # ноутбук
 #     12
 
-import re # import regex
+import re  # import regex
 
-def isCyrillic(text): # create func fo boolean ru(true) or en(false)   
-	return bool(re.search('[а-яА-Я]', text)) # 
-points_en = {1:'AEIOULNSTR', # creat dict fo price of symbol (en)
-      	2:'DG',
-      	3:'BCMP',
-      	4:'FHVWY',
-      	5:'K',
-      	8:'JZ',
-      	10:'QZ'}
-points_ru = {1:'АВЕИНОРСТ', # creat dict fo price of symbol (ru)
-      	2:'ДКЛМПУ',
-      	3:'БГЁЬЯ',
-      	4:'ЙЫ',
-      	5:'ЖЗХЦЧ',
-      	8:'ШЭЮ',
-      	10:'ФЩЪ'}
-text = input('Input text en or ru lang:\n').upper() # print text terminal
-if isCyrillic(text): # condition in func isCyrillic
-	print(f'Summ of price symbol: {sum([k for i in text for k, v in points_ru.items() if i in v])}')
+
+def isCyrillic(text):  # create func fo boolean ru(true) or en(false)
+    return bool(re.search('[а-яА-Я]', text))
+
+
+points_en = {1: 'AEIOULNSTR',  # creat dict fo price of symbol (en)
+             2: 'DG',
+             3: 'BCMP',
+             4: 'FHVWY',
+             5: 'K',
+             8: 'JZ',
+             10: 'QZ'}
+points_ru = {1: 'АВЕИНОРСТ',  # creat dict fo price of symbol (ru)
+             2: 'ДКЛМПУ',
+             3: 'БГЁЬЯ',
+             4: 'ЙЫ',
+             5: 'ЖЗХЦЧ',
+             8: 'ШЭЮ',
+             10: 'ФЩЪ'}
+text = input('Input text en or ru lang:\n').upper()  # print text terminal
+if isCyrillic(text):  # condition in func isCyrillic
+    print(
+        f'Summ of price symbol: {sum([k for i in text for k, v in points_ru.items() if i in v])}')
 else:
-	print(f'Summ of price symbol: {sum([k for i in text for k, v in points_en.items() if i in v])}')
+    print(
+        f'Summ of price symbol: {sum([k for i in text for k, v in points_en.items() if i in v])}')
